2fde508e6779fff0f2f2692bd2add5e88721336c
Luffyreals <hoangtungcnh@gmail.com>
2026-04-23 20:39:39 +0700

    update room interaction

--- Libgdx/core/src/main/java/com/gnivol/game/system/interaction/RoomInteractionHandler.java
+++ Libgdx/core/src/main/java/com/gnivol/game/system/interaction/RoomInteractionHandler.java
@@ -128,7 +128,15 @@ public class RoomInteractionHandler implements InteractionCallback {
             return;
         }
 
-        // 6. Bồn rửa mặt (Minigame Laser)
+        if ("plush_toy".equals(id)) {
+            if (screen.getPuzzleManager().isPuzzleSolved("puzzle_sliding_marble")) {
+                screen.showNotification("Bạn đã giải mã xong bí mật của gấu bông.", Color.LIGHT_GRAY);
+            } else {
+                screen.getPuzzleManager().openPuzzle("puzzle_sliding_marble");
+            }
+            return;
+        }
+        
         if ("sink".equals(id)) {
             if (screen.getPuzzleManager().isPuzzleSolved("puzzle_laser")) {
                 screen.showNotification("Mạch điện đã nối xong. Nước chảy bình thường.", Color.LIGHT_GRAY);
@@ -138,15 +146,6 @@ public class RoomInteractionHandler implements InteractionCallback {
             return;
         }
 
-        // 7. Gấu bông (Minigame Trượt Bi)
-        if ("plush_toy".equals(id)) {
-            if (screen.getPuzzleManager().isPuzzleSolved("puzzle_sliding_marble")) {
-                screen.showNotification("Bạn đã giải mã xong bí mật của gấu bông.", Color.LIGHT_GRAY);
-            } else {
-                screen.getPuzzleManager().openPuzzle("puzzle_sliding_marble");
-            }
-            return;
-        }
 
         // 8. Xử lý chung cho Dialogue, Overlay và Thought
         handleGenericInteractions(obj);
