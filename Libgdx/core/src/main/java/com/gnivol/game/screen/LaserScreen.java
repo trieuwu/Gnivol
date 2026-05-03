bf9e917ffa98363d3658a993c0c3433c0c776639
Luffyreals <hoangtungcnh@gmail.com>
2026-05-03 15:25:21 +0700

    update json

--- Libgdx/core/src/main/java/com/gnivol/game/screen/LaserScreen.java
+++ Libgdx/core/src/main/java/com/gnivol/game/screen/LaserScreen.java
@@ -215,12 +215,6 @@ public class LaserScreen extends BaseScreen {
             game.getScreenFader().startFadeIn();
             LaserScreen.this.dispose();
 
-            if (previousScreen instanceof GameScreen) {
-                ((GameScreen) previousScreen).showNotification(
-                    "Minigame Solved! Nhận được Cà vạt.",
-                    Color.GREEN
-                );
-            }
         });
 
         if (game.getAutoSaveManager() != null) {
